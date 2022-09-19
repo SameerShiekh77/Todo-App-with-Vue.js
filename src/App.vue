502c25be13f176c4aadad7777d47b79557377db7
SameerShiekh77 <muhammadsameer.css@gmail.com>
2022-09-19 17:16:44 +0500

    create, delete and retrive work completed

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+
+<template>
+<Todo />
+</template>
+
+
+<script>
+import Todo from './components/Todo.vue'
+export default{
+  components:{
+    Todo
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,96 @@
+<template>
+    <div>
+        <h1>Todo App</h1>
+
+        <form @submit="submitData">
+            <input required v-model="tasks.username" type="text" placeholder="User Name"> <br><br>
+            <input required v-model="tasks.task" type="text" placeholder="Enter your Task"> <br><br>
+            <input required v-model="tasks.status" type="radio" name="status" value="pending"> Pending 
+            <input required v-model="tasks.status" type="radio" name="status" value="completed"> Completed <br><br>
+            <button>Add Task</button>
+        </form>
+        <br><br>
+<div>
+    <table border="1px">
+        <tr>
+            <th>User Name</th>
+            <th>Task</th>
+            <th>Status</th>
+            <th colspan="2">Actions</th>
+        </tr>
+
+        <tr v-for="item in list" :key="item.id">
+            <td>{{item.username}}</td>
+            <td>{{item.task}}</td>
+            <td>{{item.status}}</td>
+            <td><button @click="updateData(item.id)">Update</button></td>
+            <td><button @click="deleteData(item.id)">Delete</button></td>
+        </tr>
+    </table>
+</div>
+    </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+export default{
+    name: 'Todo',
+    data(){
+        return{
+            tasks:{
+
+                username: null,
+                task: null,
+                status: null,
+            },
+            list: null
+
+        }
+    },
+    methods:{
+        getData(){
+            axios.get('http://localhost:3000/posts/')
+            .then(resp=>{
+                this.list = resp.data
+            })
+        },
+        submitData(e)
+        {
+            axios.post("http://localhost:3000/posts", this.tasks)
+            .then((resp)=>{
+                console.log(resp.data);
+                this.getData();
+                
+            })
+            this.tasks.username = "";
+                this.tasks.task = "";
+                this.tasks.status = "";
+            e.preventDefault();
+        },
+        updateData(id){
+            axios.get('http://localhost:3000/posts/'+id)
+            .then(resp=>{
+                this.tasks.username = resp.data.username
+                this.tasks.status= resp.data.status
+                this.tasks.task = resp.data.task
+            })
+        },
+     
+
+        deleteData(id)
+        {
+            axios.delete('http://localhost:3000/posts/'+id)
+            .then(rep=>{
+                alert("Task has Deleted");
+                this.getData();
+            })
+        }
+    },
+    mounted()
+    {
+        this.getData();
+    }
+
+}
+</script>
